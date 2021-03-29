89801cc3b203a94c4eb90d0e9ed3d86d079eae59
Mike Lambert <lambert8@illinois.edu>
2021-03-29 16:39:43 -0500

    Update test scripts to pass in envvars from the host

--- consumers/influxdb/recv.sh
+++ consumers/influxdb/recv.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run --net=host -it --rm event-worker:influxdb
+docker run --net=host -it --rm \
+  -e RABBITMQ_URI="${RABBITMQ_URI:-amqp://guest:guest@localhost/%2F}" \
+  -e RABBITMQ_EXCHANGENAME="${RABBITMQ_EXCHANGENAME:-clowder.metrics}" \
+  -e RABBITMQ_QUEUENAME="${RABBITMQ_QUEUENAME:-events.influxdb}" \
+  -e INFLUXDB_HOST="${INFLUXDB_HOST:-localhost}" \
+  -e INFLUXDB_PORT="${INFLUXDB_PORT:-8086}" \
+  -e INFLUXDB_USER="${INFLUXDB_USER:-''}" \
+  -e INFLUXDB_PASSWORD="${INFLUXDB_PASSWORD:-''}" \
+  -e INFLUXDB_DATABASE="${INFLUXDB_DATABASE:-clowder}" \
+  -e INFLUXDB_MEASUREMENTS="${INFLUXDB_MEASUREMENTS:-events}" \
+  event-worker:influxdb
