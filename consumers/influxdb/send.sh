fdf7ecb0c88604aace83b5c339eb1357588fa0ea
Mike Lambert <lambert8@illinois.edu>
2021-03-29 16:50:21 -0500

    Consistency is important

--- consumers/influxdb/send.sh
+++ consumers/influxdb/send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --net=host -it --rm \
-  -e RABBITMQ_URI="${RABBITMQ_URI:-amqp://guest:guest@rabbitmq/%2F}" \
+  -e RABBITMQ_URI="${RABBITMQ_URI:-amqp://guest:guest@localhost/%2F}" \
   -e RABBITMQ_EXCHANGENAME="${RABBITMQ_EXCHANGENAME:-clowder.metrics}" \
   -e RABBITMQ_QUEUENAME="${RABBITMQ_QUEUENAME:-events.influxdb}" \
   event-worker:influxdb /usr/src/emit_event.py $@
