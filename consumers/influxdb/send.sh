1f3dcd569f9867d7346e634cf6faba6d476ca124
Mike Lambert <lambert8@illinois.edu>
2021-03-29 20:02:45 -0500

    Update test scripts to read from an .env file

--- consumers/influxdb/send.sh
+++ consumers/influxdb/send.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-docker run --net=host -it --rm \
-  -e RABBITMQ_URI="${RABBITMQ_URI:-amqp://guest:guest@localhost/%2F}" \
-  -e RABBITMQ_EXCHANGENAME="${RABBITMQ_EXCHANGENAME:-clowder.metrics}" \
-  -e RABBITMQ_QUEUENAME="${RABBITMQ_QUEUENAME:-events.influxdb}" \
-  event-worker:influxdb /usr/src/emit_event.py $@
+docker run --net=host -it --rm --env-file .env event-worker:influxdb /usr/src/emit_event.py $@
